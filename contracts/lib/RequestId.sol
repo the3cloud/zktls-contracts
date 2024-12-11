e91c1d714abbae459697738bab5589219820ed6b
steve-the3cloud <steve@the3.cloud>
2024-12-12 02:13:06 +0800

    add sp1 verifier

--- contracts/lib/RequestId.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
-
-library RequestId {
-    function compute(address contract_, address account, uint256 nonce) internal pure returns (bytes32) {
-        return keccak256(abi.encode(contract_, account, nonce));
-    }
-}
