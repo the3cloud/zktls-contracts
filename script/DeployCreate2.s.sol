4128912565753506b1bd9a348f00ceb02ed43108
steve-the3cloud <steve@the3.cloud>
2024-12-12 11:49:28 +0800

    add tests

---
+++ script/DeployCreate2.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+import {Create2Deployer} from "../contracts/Create2Deployer.sol";
+
+import {DeployRecorder} from "./DeployRecorder.sol";
+
+contract Deploy is Script, DeployRecorder {
+    function run() external {
+        vm.startBroadcast();
+
+        Create2Deployer deployer = new Create2Deployer();
+
+        console.log("Create2Deployer deployed at", address(deployer));
+
+        saveCreate2Deployer(configPath(), address(deployer));
+
+        vm.stopBroadcast();
+    }
+}
