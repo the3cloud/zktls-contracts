cc9db7a9f6acaad6d97c8635d90ddcca519b1006
steve-the3cloud <steve@the3.cloud>
2024-12-20 01:04:28 +0800

    add deploy script for sp1

--- contracts/verifiers/RISC0Verifier.sol
+++ contracts/verifiers/RISC0Verifier.sol
@@ -4,19 +4,14 @@ pragma solidity ^0.8.20;
 import {IProofVerifier} from "../interfaces/IProofVerifier.sol";
 import {IRiscZeroVerifier} from "risc0-ethereum/IRiscZeroVerifier.sol";
 
-contract Risc0Prover is IProofVerifier {
+contract RISC0Verifier is IProofVerifier {
     address public immutable risc0Verifier;
 
     bytes32 public immutable imageId;
 
-    uint256 public immutable nativeVerifyGas;
-    uint256 public immutable paymentVerifyFee;
-
-    constructor(address risc0Verifier_, bytes32 imageId_, uint256 nativeVerifyGas_, uint256 paymentVerifyFee_) {
+    constructor(address risc0Verifier_, bytes32 imageId_) {
         risc0Verifier = risc0Verifier_;
         imageId = imageId_;
-        nativeVerifyGas = nativeVerifyGas_;
-        paymentVerifyFee = paymentVerifyFee_;
     }
 
     function verifyProof(bytes calldata publicValues_, bytes calldata proofBytes_) external view {
